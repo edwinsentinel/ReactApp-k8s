f5b816672870153876d054079d751dd7cf9b2b75
Edwin <waweruedwin2@gmail.com>
2019-10-17 15:00:07 +0300

    first deployment

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+docker build -t sentinelwawesh/multi-client:latest -t sentinelwawesh/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sentinelwawesh/multi-server:latest -t sentinelwawesh/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sentinelwawesh/multi-worker:latest -t sentinelwawesh/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push sentinelwawesh/multi-client:latest
+docker push sentinelwawesh/multi-worker:latest
+docker push sentinelwawesh/multi-server:latest
+docker push sentinelwawesh/multi-client:$SHA
+docker push sentinelwawesh/multi-worker:$SHA
+docker push sentinelwawesh/multi-server:$SHA
+kubectl apply  -f k8s
+kubectl set image deployments/server-deployment server= sentinelwawesh/multi-server:$SHA
+kubectl set image deployments/client-deployment client= sentinelwawesh/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker= sentinelwawesh/multi-worker:$SHA
+
